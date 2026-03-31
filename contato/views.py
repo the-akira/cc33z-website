9d6094dcc8d3268127387f109d4365d4c9b52104
Gabriel Felippe <gabrielfelippe@outlook.com>
2026-03-30 22:53:11 -0300

    View de Contato Atualizada

--- contato/views.py
+++ contato/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Contato
 
 def contato(request):
@@ -8,4 +9,6 @@ def contato(request):
         mensagem = request.POST.get('mensagem')
         contato = Contato(nome=nome, email=email, mensagem=mensagem)
         contato.save()
+        messages.success(request, 'Mensagem enviada com sucesso!')
+        return redirect('contato')  # nome da sua URL
     return render(request,'contato.html')
